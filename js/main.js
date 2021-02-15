d7bf61da27b37f2a8a23c682fdb89d8209c4092d
Milad <milad_abdollahnia@yahoo.com>
2021-02-15 20:20:43 +0330

    Some SEO stuff, new version of icons, new style for the contact section

--- js/main.js
+++ js/main.js
@@ -101,17 +101,4 @@
 		});
 	}
 
-	/*--/ Testimonials owl /--*/
-	$('#testimonial-mf').owlCarousel({
-		margin: 20,
-		autoplay: true,
-		autoplayTimeout: 4000,
-		autoplayHoverPause: true,
-		responsive: {
-			0: {
-				items: 1,
-			}
-		}
-	});
-
 })(jQuery);
